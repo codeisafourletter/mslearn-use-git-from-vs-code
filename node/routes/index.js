8ec200ac766d42076e85e7c1812055d1de2284e7
Trevor Hedley <25409828+codeisafourletter@users.noreply.github.com>
2022-01-27 13:20:47 -0600

    test2

--- node/routes/index.js
+++ node/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. 2 */
+/* GET home page. 2 3*/
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Hello Git!' });
 });
